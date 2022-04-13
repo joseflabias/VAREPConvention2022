928c4839c832b2cab5b327aeafa0c59555953f08
David Santana <davidsant409@csu.fullerton.edu>
2022-04-13 09:12:52 -0700

    adding custom screen

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-export const applyCustomCode = externalCodeSetup => {
-	// call custom code api here
+
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import QRCode from "react-native-qrcode-svg";
+import ScheduleScreen from "./Screens/Schedule";
+
+export const applyCustomCode = (externalCodeSetup) => {
+  // call custom code api here
+  ScheduleScreen.navigationOptions = {
+    title: "SCHEDULE",
+    headerStyle: {
+      backgroundColor: "#C0223E",
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      fontFamily: "Liberator",
+      fontSize: 30,
+    },
+  };
+  externalCodeSetup.navigationApi.addNavigationRoute(
+    "ScheduleScreen",
+    "ScheduleScreen",
+    ScheduleScreen,
+    "Main"
+  );
+  externalCodeSetup.navigationApi.replaceScreenComponent(
+    "ProfilePhotos",
+    (props) => {
+      return (
+        <View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              props.navigation.navigate("TestScreen");
+            }}
+          >
+            <Text>Press Here</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+  );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+  },
+});
+
