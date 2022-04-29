b0c9c1c015fdf6889b9fc54b20833926e5bc564b
David Santana <davidsantana409@yahoo.com>
2022-04-29 09:42:36 -0700

    Update EventActivity.js

--- Screens/EventActivity.js
+++ Screens/EventActivity.js
@@ -7,26 +7,6 @@ import { DEVICE_WIDTH, DEVICE_HEIGHT } from "@src/styles/global";
 
 import { ErrorBoundary } from "react-native-error-boundary";
 
-const myErrorHandler = (error) => {
-  // Do something with the error
-  // E.g. reporting errorr using sentry ( see part 3)
-  console.log(error);
-};
-function ErrorFallback({ resetErrorBoundary }) {
-  return (
-    <View style={[styles.container]}>
-      <View>
-        <Text> Something went wrong: </Text>
-        <Button title="try Again" onPress={resetErrorBoundary} />
-      </View>
-    </View>
-  );
-}
-export const ErrorHandler = ({ children }) => (
-  <ErrorBoundary FallbackComponent={ErrorFallback} onError={myErrorHandler}>
-    {children}
-  </ErrorBoundary>
-);
 
 export default function EventActivity(props) {
   const [randomNumber, setRandomNumber] = useState(
