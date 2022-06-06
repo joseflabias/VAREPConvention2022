4f58c45352f4d049bd54d83fc5bb433734975ef2
David Santana <davidsant409@csu.fullerton.edu>
2022-06-06 09:33:50 -0700

    adding updates

--- Components/Schedule/AllSessions.js
+++ Components/Schedule/AllSessions.js
@@ -1,21 +1,29 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { StyleSheet, Text, View, SectionList } from "react-native";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+const Dates = {
+  sat: 0,
+  sun: 1,
+  mon: 2,
+};
 
 export default function AllSessions({ date }) {
   const [allSessions, setAllSessions] = useState();
-
   axios.get("https://varep.net/wp-json/varepcc/v1/sessions").then((resp) => {
-    console.log(resp.data);
+    setAllSessions(resp.data.sessions);
   });
-  const renderItem = ({ item }) => {
-    console.log(item);
-    return (
-      <View>
-        <Text>{item.title}</Text>
-      </View>
-    );
-  };
+  useEffect(() => {
+    if (allSessions) {
+      console.log(allSessions[date][0]);
+    }
+  }, [date]);
+
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
   return (
     <View>
       <Text>Hello</Text>
