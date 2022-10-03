d86c1b50d192bda653b99a0fdb47be96296aa093
David Santana <davidsant409@csu.fullerton.edu>
2022-10-03 16:13:08 -0700

    finished app. last commit

--- Screens/ResourceScreen.js
+++ Screens/ResourceScreen.js
@@ -1,10 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { WebView } from "react-native-webview";
 
 export default function ResourceScreen() {
+  const INJECTED_JAVASCRIPT = `(function() {
+    const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta);
+  })();`;
+
   return (
-    <View>
-      <Text>ResourceScreen</Text>
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{ uri: "https://convention.varep.net/app-resource/" }}
+        scrollEnabled={false}
+        injectedJavaScript={INJECTED_JAVASCRIPT}
+        onMessage={() => {}}
+      />
     </View>
   );
 }
