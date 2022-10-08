bc8972d0e79fc88c77c8fc43fd648515f9ea9c0c
joseflabias <49792168+joseflabias@users.noreply.github.com>
2022-10-08 09:49:31 -0500

    Update utils.js
    changes to fix AM to PM in 12:00

--- Screens/utils.js
+++ Screens/utils.js
@@ -1,6 +1,6 @@
 export function get12HourTime(date) {
-    let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours()
+    let hours = date.getHours() >= 12 ? date.getHours() - 12 : date.getHours()
     let minutes = date.getMinutes() == 0 ? "00" : date.getMinutes()
-    let p = date.getHours() > 12 ? " PM" : " AM"
+    let p = date.getHours() >= 12 ? " PM" : " AM"
     return hours + ":" + minutes + p
 }
