123fe296cea51dd7f2518304594afbe238de95ea
joseflabias <49792168+joseflabias@users.noreply.github.com>
2022-10-08 22:00:04 -0500

    Update utils.js
    another fixed to solved 00 in app

--- Screens/utils.js
+++ Screens/utils.js
@@ -1,5 +1,11 @@
 export function get12HourTime(date) {
-    let hours = date.getHours() >= 12 ? date.getHours() - 12 : date.getHours()
+  if (mytime==12){
+        var hours = 12;
+    }
+    else{
+        var hours = mytime >= 12 ? mytime - 12 : mytime
+    }
+    
     let minutes = date.getMinutes() == 0 ? "00" : date.getMinutes()
     let p = date.getHours() >= 12 ? " PM" : " AM"
     return hours + ":" + minutes + p
